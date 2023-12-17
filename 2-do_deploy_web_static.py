3ac23fdbd29f404c5f1a3b0912b9ce1ce4ffd202
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-12-17 10:38:00 -0800

    up

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,50 +15,46 @@ env.hosts = ["18.210.28.31", "3.236.121.230"]
 def do_pack():
     """ generate .tgz file"""
     archived = dt.utcnow()
-    if path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+    if not path.isdir("versions"):
+        if local("mkdir -p versions").failed:
             return None
     p = "versions/web_static_{}{}{}{}{}{}.tgz".format(
         archived.year, archived.month, archived.day,
         archived.hour, archived.minute, archived.second)
-    if local("tar -cvzf {} web_static/".format(
-            p)).failed is True:
+    if local("tar -cvzf {} web_static/".format(p)).failed:
         return None
     return p
 
 
 def do_deploy(archive_path):
     """ Deploy files to servers"""
-    print("archive_path")
-    if path.isfile("{}".format(archive_path)) is False:
+    print(archive_path)
+    if not path.isfile(archive_path):
         return False
-    result = put("{}".format(
-        archive_path), "/tmp/{}".format(
-            archive_path.split("/")[1]))
-    if result.failed is True:
+    result = put(archive_path, "/tmp/{}".format(archive_path.split("/")[1]))
+    if result.failed:
         return False
     extract_path = archive_path.split("/")[1].split(".")[0]
-    if run("rm -rf /data/web_static/releases/{}/".format(
-            extract_path)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".format(extract_path)).failed:
         return False
-    if run(
-            "mkdir -p /data/web_static/releases/{}".format(
-                extract_path)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}".format(extract_path)).failed:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            archive_path.split("/")[1], extract_path)).failed is True:
+            archive_path.split("/")[1], extract_path)).failed:
         return False
-    if run("rm -rf /tmp/{}".format(archive_path.split("/")[1])).failed is True:
+    if run("rm -rf /tmp/{}".format(archive_path.split("/")[1])).failed:
         return False
     part1 = "mv /data/web_static/releases/{}/web_static/*".format(extract_path)
     part2 = "/data/web_static/releases/{}/".format(extract_path)
     mv = "{} {}".format(part1, part2)
-    if run(mv).failed is True:
+    if run(mv).failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        pass
-    if run(
-        "ln -sf /data/web_static/releases/{}/ /data/web_static/current"
-            .format(extract_path)).failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    return True
+    if run("ln -sf /data/web_static/releases/{}/ /data/web_static/current".format(extract_path)).failed:
+        return False
+    
+    # Additional step to check if the HTML file is available
+    check_html = run("curl -s -o /dev/null -w \"%{{http_code}}\" http://{}{}".format(env.hosts[0], '/hbnb_static/0-index.html'))
+    return check_html == "200"
+
