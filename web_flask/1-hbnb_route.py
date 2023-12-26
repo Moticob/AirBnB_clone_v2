cbd3e332508c247bb04b2a0ad2c10398e6e86c42
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-12-26 07:09:15 -0800

    up

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,7 +4,6 @@ Starts a Flask web application.
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -19,7 +18,5 @@ def hbnb():
     """Displays 'HBNB'"""
     return 'HBNB'
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
